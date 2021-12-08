7583b74b8709e536603ce91428c70491c52bf3bb
ricardo de melo sousa <ricardo_melo_sousa@hotmail.com>
2021-12-08 00:18:51 -0300

    subindo microserviço

---
+++ GeekBurger.StoreCatalog/GeekBurger.StoreCatalog.Contract/Class1.cs
@@ -0,0 +1,35 @@
+﻿namespace GeekBurger.StoreCatalog.Contract
+{
+    public class Product
+    {
+        public string StoreName { get; set; }
+        public Guid ProductId { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public List<Item> Items { get; set; }
+        public decimal Price { get; set; }
+
+
+    }
+
+    public class Item
+    {
+        public Guid ItemId { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ProductChanged
+    {
+        public ProductState State { get; set; }
+        public Product Product { get; set; }
+    }
+
+    public enum ProductState
+    {
+        Deleted = 2,
+        Modified = 3,
+        Added = 4
+    }
+
+
+}
